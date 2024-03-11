3bd9c9e8b97a929f180e16cf16fc0d9d6e523b1d
HarDhillon <har7173@gmail.com>
2024-03-11 08:24:42 +0000

    Adds functionality to allow updating a product

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator')
 const Post = require('../models/post')
-const post = require('../models/post')
+const fs = require('fs')
+const path = require('path')
 
 exports.getPosts = (req, res, next) => {
     Post.find()
@@ -82,4 +83,64 @@ exports.getPost = (req, res, next) => {
             }
             next(err)
         })
+}
+
+exports.updatePost = (req, res, next) => {
+
+    // Error check
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect')
+        error.statusCode = 422
+        throw error
+    }
+
+    const { title, content } = req.body
+    const postId = req.params.postId
+    let imageUrl = req.body.image
+
+    if (req.file) {
+        imageUrl = req.file.path
+    }
+    // If imageUrl still not set
+    if (!imageUrl) {
+        const error = new Error('No file picked')
+        error.statusCode = 422
+        throw error
+    }
+
+    Post.findById(postId)
+        .then(post => {
+            // If no post
+            if (!post) {
+                const error = new Error('Could not locate post')
+                error.statusCode = 404
+                throw (error)
+            }
+            // If imageUrl is changed, new file is uploaded
+            if (imageUrl !== post.imageUrl) {
+                clearImage(post.imageUrl)
+            }
+
+            post.title = title
+            post.imageUrl = imageUrl
+            post.content = content
+
+            return post.save()
+        })
+        // Update success
+        .then(result => {
+            res.status(200).json({ message: 'Post updated', post: result })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
+
+const clearImage = filepath => {
+    filePath = path.join(__dirname, '..', filepath)
+    fs.unlink(filepath, err => console.log(err))
 }

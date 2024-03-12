dee10410be1fb4618f57a9e732979d2f644255f5
HarDhillon <har7173@gmail.com>
2024-03-12 17:12:58 +0000

    - Update creator field in Post model to reference User
    - Update feed controller so that when saving a post, save the post to the user document as well

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,8 @@
 const { validationResult } = require('express-validator')
+
 const Post = require('../models/post')
+const User = require('../models/user')
+
 const fs = require('fs')
 const path = require('path')
 
@@ -54,19 +57,29 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path
     const title = req.body.title
     const content = req.body.content
+    let creator
 
     const post = new Post({
         title,
         content,
         imageUrl: imageUrl,
-        creator: { name: "Har" }
+        creator: req.userId
     })
 
     post.save()
         .then(result => {
+            return User.findById(req.userId)
+        })
+        .then(user => {
+            creator = user
+            user.posts.push(post)
+            return user.save()
+        })
+        .then(result => {
             res.status(201).json({
                 message: 'Post created successfully',
-                post: result
+                post: post,
+                creator: { _id: creator._id, name: creator.name }
             })
         })
         .catch(err => {

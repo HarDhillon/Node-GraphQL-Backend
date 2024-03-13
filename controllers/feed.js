0256f17b696882649502057f9cf3c9b0fb820227
HarDhillon <har7173@gmail.com>
2024-03-13 14:03:57 +0000

    Remove post from user document if post is deleted

--- controllers/feed.js
+++ controllers/feed.js
@@ -198,6 +198,14 @@ exports.deletePost = (req, res, next) => {
             return Post.findOneAndDelete(postId)
         })
         .then(result => {
+            return User.findById(req.userId)
+        })
+        .then(user => {
+            // remove the post that has been deleted from the user as well
+            user.posts.pull(postId)
+            return user.save()
+        })
+        .then(result => {
             res.status(200).json({ nessage: 'Deleted post' })
         })
         .catch(err => {
